40f1058aa41fcd28d4b5a1e9f103765c57792fef
nouble-cpu <alesarozin@gmail.com>
2026-03-05 11:12:42 +0300

    main.py

--- Разделение игроков на две команды/main.py
+++ Разделение игроков на две команды/main.py
@@ -1,9 +1,8 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 
-# индекс середины
 middle = len(list_players) // 2             #Находим середину
 
-first_team = list_players[:middle]          #Делим с 0 индекса до середины
-second_team = list_players[middle:]         #Делим с середины до конца
-print(first_team)                           #Вывод 1 порловины
-print(second_team)                          #Вывод 2 порловины
+team_f = list_players[:middle]              #Делим с 0 индекса до середины
+team_s = list_players[middle:]              #Делим с середины до конца
+print(team_f)                               #Вывод 1 порловины
+print(team_s)                               #Вывод 2 порловины
